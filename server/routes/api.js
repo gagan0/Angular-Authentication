e96d3fe6699f5659e5993b84e11345ec5e074104
Gagan (Mac) <gagan.maheshwari@trakkerz.com>
2019-08-01 15:21:51 +0530

    formatting changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("../models/User");
+const User = require("../models/User").default;
 
 const router = express.Router();
 
@@ -8,74 +8,74 @@ const db = "mongodb+srv://gagan:gagan@cluster0-muiyi.mongodb.net/AngularAuth?ret
 
 mongoose.connect(db, error =>
 {
-    if (error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        console.log("asdf");
-    }
+	if (error)
+	{
+		console.log(error);
+	}
+	else
+	{
+		console.log("asdf");
+	}
 });
 
 router.get("/", (request, response) =>
 {
-    response.send("From API Route");
+	response.send("From API Route");
 });
 
 router.post("/register", (request, response) =>
 {
-    let userData = request.body;
-    let user = new User(userData);
+	let userData = request.body;
+	let user = new User(userData);
 
-    user.save((error, registeredUser) =>
-    {
-        if (error)
-        {
-            console.log(error);
-        }
-        else
-        {
-            response.status(200).send(registeredUser);
-        }
-    });
+	user.save((error, registeredUser) =>
+	{
+		if (error)
+		{
+			console.log(error);
+		}
+		else
+		{
+			response.status(200).send(registeredUser);
+		}
+	});
 });
 
 router.post("/login", (request, response) =>
 {
-    console.log("3edc");
-    let userData = request.body;
-    console.log(userData);
+	console.log("3edc");
+	let userData = request.body;
+	console.log(userData);
 
-    User.findOne(
-        {
-            email: userData.email
-        },
-        (error, user) =>
-        {
-            if (error)
-            {
-                console.log("123");
-                console.log(error);
-                console.log("456");
-            }
-            else
-            {
-                if (!user)
-                {
-                    response.status(401).send("Invalid email");
-                }
-                else if (user.password !== userData.password)
-                {
-                    response.status(401).send("Invalid password");
-                }
-                else
-                {
-                    response.send(200).send(user);
-                }
-            }
-        }
-    );
+	User.findOne(
+		{
+			email: userData.email
+		},
+		(error, user) =>
+		{
+			if (error)
+			{
+				console.log("123");
+				console.log(error);
+				console.log("456");
+			}
+			else
+			{
+				if (!user)
+				{
+					response.status(401).send("Invalid email");
+				}
+				else if (user.password !== userData.password)
+				{
+					response.status(401).send("Invalid password");
+				}
+				else
+				{
+					response.send(200).send(user);
+				}
+			}
+		}
+	);
 });
 
 module.exports = router;
